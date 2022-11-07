93528c0ae284ae87b55b74b2e5090e0e23ab842f
Peter <petr.liauos@gmail.com>
2022-11-07 10:36:58 +0800

    Finally works

---
+++ Practical 2 - FileIO/My Telephone Directory - Wk2 Qn1 VSCode/Week 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+while(true)
+{
+    Console.Write("Enter name: ");
+    string name= Console.ReadLine();
+    if (name == "Exit") break;
+    Console.Write("Enter Phone number ");
+    int phoneno = Convert.ToInt32(Console.ReadLine());
+    string data = name + "," + phoneno;
+    using (StreamWriter sw = new StreamWriter("PhoneDirectory.csv", true))
+    {
+        sw.WriteLine(data);
+    }
+}
